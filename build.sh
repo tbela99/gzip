b3fbacd1920dcba0976cfb63c35bb3ee76277725
Thierry Bela <bnanga@gmail.com>
2017-07-03 16:54:32 -0400

    Missing files

---
+++ build.sh
@@ -0,0 +1,9 @@
+cat localforage.js > localforage-all.js
+cat localforage-getitems.js >> localforage-all.js
+cat localforage-setitems.js >> localforage-all.js
+cat localforage-removeitems.js >> localforage-all.js
+uglifyjs --compress --mangle -- localforage-all.js > localforage-all.min.js
+uglifyjs --compress --mangle -- load-worker.js > load-worker.min.js
+uglifyjs --compress --mangle -- worker.js > worker.min.js
+php -r "echo hash_file('crc32', 'load-worker.js');" > worker.checksum
+php -r "echo hash_file('crc32', 'load-worker.min.js');" > worker.min.checksum
